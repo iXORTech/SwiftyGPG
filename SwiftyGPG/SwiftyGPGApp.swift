3208c9e34ad901428ef46a5860a0f3157096b28f
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-06-12 20:42:43 -0400

    Initial Commit

---
+++ SwiftyGPG/SwiftyGPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyGPGApp.swift
+//  SwiftyGPG
+//
+//  Created by Cubik65536 on 2024-06-12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyGPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftyGPGTests/SwiftyGPGTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyGPGTests.swift
+//  SwiftyGPGTests
+//
+//  Created by Cubik65536 on 2024-06-12.
+//
+
+import Testing
+@testable import SwiftyGPG
+
+struct SwiftyGPGTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
